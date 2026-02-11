0c0d60895aa0f688d246314f6c381cd6612a6739
Kazu <kazuki.ino01@gmail.com>
2026-02-11 16:26:26 +0900

    feat: アーティスト登録フォームの実装とAPI連携

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 interface Artist {
   id: number;
@@ -6,7 +6,12 @@ interface Artist {
 }
 
 function App() {
+
+  // メモ用紙たち（useState）
   const [artists, setArtists] = useState<Artist[]>([]);
+  const [name, setName] = useState("");
+  const [group_name, setGroup_name] = useState("");
+  const [member_color, setMember_color] = useState("");
 
   useEffect(() => {
     // rails apiからデータを取得する
@@ -15,9 +20,77 @@ function App() {
     .then(data => setArtists(data))
   }, [])
 
+  // 送信ボタンが押された時の処理
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // 送信するデータをオブジェクトで作る
+    const artistData = {
+      artist: {
+        name: name,
+        group_name: group_name,
+        member_color: member_color
+      }
+    }
+
+    // fetchでrailsに送る
+    fetch("http://localhost:3000/artists", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(artistData)
+    })
+    .then(res => {
+      if (res.ok) {
+        // 成功した場合
+        // 入力欄のリセット
+        setName("");
+        setGroup_name("");
+        setMember_color("");
+        console.log("登録成功");
+        // 中身を解析
+        return res.json()
+        // リスト更新
+        .then(data => {
+          setArtists([...artists, data]);
+        })
+      } else {
+        // 失敗した場合
+        console.log("登録失敗");
+      }
+    })
+  }
+
   return (
     <div style={{ padding: '20px' }}>
       <h1>推しメン一覧</h1>
+
+      <form onSubmit={handleSubmit}>
+        {/* アーティスト名の入力欄 */}
+        <input
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder="名前を入力してください"
+          type="text"
+        />
+        {/* グループ名の入力欄 */}
+        <input
+          value={group_name}
+          onChange={e => setGroup_name(e.target.value)}
+          placeholder="グループ名を入力してください"
+          type="text"
+        />
+        {/* メンバーカラーの入力欄 */}
+        <input
+          value={member_color}
+          onChange={e => setMember_color(e.target.value)}
+          placeholder="メンバーカラーを入力してください"
+          type="text"
+        />
+        <button type="submit">登録</button>
+      </form>
+
       {artists.length === 0 ? (
         <p>データがまだありません。railsコンソールで登録してみてね！</p>
       ) : (
